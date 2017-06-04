0b2d2c184a915ed3e091e77e3853ac4ec38abf05
leaderanalytics <leaderanalytics@outlook.com>
2017-06-04 14:49:14 -0700

    Add Project

---
+++ AC.XamDemo/AC.XamDemo/AC.XamDemo.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AC.XamDemo.Droid
+{
+	[Activity (Label = "AC.XamDemo", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar; 
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new AC.XamDemo.App ());
+		}
+	}
+}
+
